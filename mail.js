93437d6930b8a29b1cd60f4c53e1c75f4bf107bb
Maxim <kodemak@gmail.com>
2013-10-15 08:34:47 +0400

    pb

---
+++ mail.js
@@ -0,0 +1,28 @@
+var nodemailer = require("nodemailer");
+var config = require('./config.json');
+
+
+function sendMail(){
+  student = config.students.pop();
+
+  var smtpTransport = nodemailer.createTransport("SMTP",{
+     service: "Gmail",
+     auth: student
+  });
+
+  smtpTransport.sendMail({
+     from: ""+student.name+" ("+student.group+") <"+student.user+">", 
+     to: config.teacher.name+" <"+config.teacher.mail+">", 
+     subject: config.teacher.sbj, 
+     text: config.teacher.msg 
+  }, function(error, response){
+     if(error){
+         console.log(error);
+     }else{
+         console.log("Message sent: "+student.name+" "+ response.message);
+     }
+     config.students.length?sendMail():process.exit();
+  });
+}
+
+sendMail();
